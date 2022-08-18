438a42b4fab4dbbd95a2186e68d3c92f39c525cd
yesifiore91 <jesicafiore1@gmail.com>
2022-08-18 15:32:39 -0300

    agrego a las vistas el login y register, agrego la hoja de estilo user.css, agrego las rutas al app, sigo editando la vista register

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 3030
 
 app.use(express.static(path.join(__dirname,"public")))
 
-app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views","home.html")))
+app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views","home.html")));
+app.get("/login",(req,res)=> res.sendFile(path.resolve(__dirname,"views","login.html")))
+app.get("/register",(req,res)=> res.sendFile(path.resolve(__dirname,"views","register.html")))
+
 
 app.listen(port,()=> console.log(`se levanto con el exito el servidor en http://localhost:${port}`))
